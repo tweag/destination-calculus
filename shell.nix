2d03b5e792d0fe4ac1e7c35269e71425518194f6
Thomas BAGREL <tomsb07@gmail.com>
2022-04-21 18:57:04 +0200

    initial commit

---
+++ shell.nix
@@ -0,0 +1,31 @@
+{ sources ? import ./nix/sources.nix # See: https://github.com/nmattia/niv and https://nix.dev/tutorials/towards-reproducibility-pinning-nixpkgs.html#dependency-management-with-niv
+, pkgs ? import sources.nixpkgs {}   # Use the pinned sources.
+}:
+
+pkgs.mkShell
+  { buildInputs = with pkgs; [
+      niv
+      ott
+      biber
+      (texlive.combine {
+        inherit (texlive)
+          # basic toolbox
+          scheme-small
+          latexmk
+          biblatex
+          unicode-math
+          lm
+          lm-math
+          preprint xcolor
+          bussproofs
+          supertabular
+          geometry
+          # minted
+          # minted fvextra catchfile xstring kvoptions fancyvrb
+          # upquote float ifplatform pdftexcmds etoolbox
+          # xcolor lineno framed
+          ;
+      })
+
+      ];
+}
